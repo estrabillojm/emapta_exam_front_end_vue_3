29655f07b8b9d774fffddd562ba174ec170815dd
John Michael Estrabillo <estrabillo.johnmichael@gmail.com>
2021-09-03 23:47:22 -0700

    revision from nested if to guard clause

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -194,50 +194,49 @@ export default {
     }
     // FORM VALIDATION
     const validateData = () => {
-      // CHECK IF THE TYPE CONTAINS A VALUE
-      if(data.value.type){
-        err.value.type = ''
-        err.value.alias = ''
-        err.value.description = ''
-        // CHECK IF THE ALIAS CONTAINS A VALUE
-        if(data.value.alias){
-          // CHECK IF THE ALIAS HAVE 3 MORE CHARACTERS
-          if(data.value.alias.length > 2){
-            // CHECK IF THE DESCRIPTION HAVE A VALUE
-            if(data.value.description){
-              // CHECK IF THE DESCRIPTION HAVE 7 MORE CHARACTERS
-              if(data.value.description.length > 7){
-                // CHECK IF THE STATE IS ABOUT TO SAVE OR UPDATE
-                if(data.value.type == '0'){
-                  
-                  let val = values.value.map(val=> val.alias.toLowerCase())
-                  if(val.includes(data.value.alias.toLowerCase()) && !editMode.value){
-                    err.value.alias = 'Alias already used in Values'
-                  }else{
-                    saveData()
-                  }
-                }else{
-                  let principle = principles.value.map(val=> val.alias.toLowerCase())
-                  if(principle.includes(data.value.alias.toLowerCase()) && !editMode.value){
-                    err.value.alias = 'Alias already used in Principles'
-                  }else{
-                    saveData()
-                  }
-                }
-              }else{
-                err.value.description = 'Alias must consist of 7 or more characters*'
-              }
-            }else{
-              err.value.description = "Description Field is required*"
-            }
-          }else{
-            err.value.alias = "Alias must consist of 3 or more characters*"
-          }
-        }else{
-          err.value.alias = "Alias Field is required*"
-        }
-      }else{
+      err.value.type = ''
+      err.value.alias = ''
+      err.value.description = ''
+
+      if(!data.value.type){
         err.value.type = "Type Field is required*"
+        return false
+      }
+      if(!data.value.alias){
+        err.value.alias = "Alias Field is required*"
+        return false
+      }
+      if(data.value.alias.length < 3){
+        err.value.alias = "Alias must consist of 3 or more characters*"
+        return false
+      }
+      if(!data.value.description){
+        err.value.description = "Description Field is required*"
+        return false
+      }
+      if(data.value.description.length < 8){
+        err.value.description = 'Alias must consist of 7 or more characters*'
+        return false
+      }
+      // CHECK IF THE TYPE IS EQUAL TO VALUES
+      if(data.value.type == '0'){
+        let val = values.value.map(val=> val.alias.toLowerCase())
+        if(val.includes(data.value.alias.toLowerCase()) && !editMode.value){
+          err.value.alias = 'Alias already used in Values'
+          return false
+        }
+        saveData()
+        return true
+      }
+      // CHECK IF THE TYPE IS EQUAL TO PRINCIPLES
+      if(data.value.type == '1'){
+        let principle = principles.value.map(val=> val.alias.toLowerCase())
+        if(principle.includes(data.value.alias.toLowerCase()) && !editMode.value){
+          err.value.alias = 'Alias already used in Principles'
+          return false
+        }
+        saveData()
+        return true;
       }
     }
     const getAllPrinciples = () => store.dispatch(`principles/getAllPrinciples`)
---
+++ src/views/Sandbox.vue
@@ -0,0 +1,75 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    setup(){
+        const validate = ()=>{
+            if(data.value.type){
+
+
+
+        err.value.type = ''
+        err.value.alias = ''
+        err.value.description = ''
+        // CHECK IF THE ALIAS CONTAINS A VALUE
+        if(data.value.alias){
+
+
+
+          // CHECK IF THE ALIAS HAVE 3 MORE CHARACTERS
+          if(data.value.alias.length > 2){
+
+
+
+            // CHECK IF THE DESCRIPTION HAVE A VALUE
+            if(data.value.description){
+
+
+
+              // CHECK IF THE DESCRIPTION HAVE 7 MORE CHARACTERS
+              if(data.value.description.length > 7){
+
+                  
+                // CHECK IF THE STATE IS ABOUT TO SAVE OR UPDATE
+                if(data.value.type == '0'){
+                  
+                  let val = values.value.map(val=> val.alias.toLowerCase())
+                  if(val.includes(data.value.alias.toLowerCase()) && !editMode.value){
+                    err.value.alias = 'Alias already used in Values'
+                  }else{
+                    saveData()
+                  }
+                }else{
+                  let principle = principles.value.map(val=> val.alias.toLowerCase())
+                  if(principle.includes(data.value.alias.toLowerCase()) && !editMode.value){
+                    err.value.alias = 'Alias already used in Principles'
+                  }else{
+                    saveData()
+                  }
+                }
+              }else{
+                err.value.description = 'Alias must consist of 7 or more characters*'
+              }
+            }else{
+              err.value.description = "Description Field is required*"
+            }
+          }else{
+            err.value.alias = "Alias must consist of 3 or more characters*"
+          }
+        }else{
+          err.value.alias = "Alias Field is required*"
+        }
+      }else{
+        err.value.type = "Type Field is required*"
+      }
+    }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
